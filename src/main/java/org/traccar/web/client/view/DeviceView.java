06fcc407835d240eac107887cbb8ae4ab2fd3e5e
Łukasz Ławniczak <lukasz.lawniczak@datamatica.pl>
2017-04-06 13:08:18 +0200

    AUTO-698 Ignition time validity period change

--- src/main/java/org/traccar/web/client/view/DeviceView.java
+++ src/main/java/org/traccar/web/client/view/DeviceView.java
@@ -41,7 +41,6 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.Event;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.AbstractImagePrototype;
 import com.google.gwt.user.client.ui.Widget;
 import com.sencha.gxt.cell.core.client.form.CheckBoxCell;
