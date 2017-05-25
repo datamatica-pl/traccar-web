c07ad706171a5c8cbb41816f59f0e85b6da99e51
Łukasz Ławniczak <lukasz.lawniczak@datamatica.pl>
2017-05-25 11:52:44 +0200

    FIX: Route point duplication, showing route points in grid (instead of geofences)

--- src/main/java/org/traccar/web/client/view/RouteDialog.java
+++ src/main/java/org/traccar/web/client/view/RouteDialog.java
@@ -16,12 +16,10 @@
 package org.traccar.web.client.view;
 
 import com.google.gwt.cell.client.Cell;
-import com.google.gwt.cell.client.ImageCell;
 import com.google.gwt.cell.client.ImageResourceCell;
 import com.google.gwt.cell.client.ValueUpdater;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
-import com.google.gwt.dom.client.EventTarget;
 import com.google.gwt.dom.client.NativeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
@@ -37,10 +35,13 @@ import com.sencha.gxt.core.client.ValueProvider;
 import com.sencha.gxt.data.shared.LabelProvider;
 import com.sencha.gxt.data.shared.ListStore;
 import com.sencha.gxt.data.shared.ModelKeyProvider;
+import com.sencha.gxt.data.shared.PropertyAccess;
 import com.sencha.gxt.widget.core.client.Window;
-import com.sencha.gxt.widget.core.client.button.TextButton;
+import com.sencha.gxt.widget.core.client.box.AlertMessageBox;
+import com.sencha.gxt.widget.core.client.box.MessageBox;
 import com.sencha.gxt.widget.core.client.event.BeforeStartEditEvent;
 import com.sencha.gxt.widget.core.client.event.BeforeStartEditEvent.BeforeStartEditHandler;
+import com.sencha.gxt.widget.core.client.event.DialogHideEvent;
 import com.sencha.gxt.widget.core.client.event.SelectEvent;
 import com.sencha.gxt.widget.core.client.form.CheckBox;
 import com.sencha.gxt.widget.core.client.form.ComboBox;
@@ -66,6 +67,7 @@ import org.gwtopenmaps.openlayers.client.LonLat;
 import org.traccar.web.client.i18n.Messages;
 import org.traccar.web.client.utils.Geocoder;
 import org.traccar.web.client.utils.Geocoder.SearchCallback;
+import org.traccar.web.shared.model.ValidationException;
 import pl.datamatica.traccar.model.Device;
 import pl.datamatica.traccar.model.GeoFence;
 import pl.datamatica.traccar.model.GeoFenceType;
@@ -76,6 +78,7 @@ public class RouteDialog implements MapPointSelectionDialog.PointSelectedListene
     interface _UiBinder extends UiBinder<Widget, RouteDialog> {}
     private static _UiBinder uiBinder = GWT.create(_UiBinder.class);
     private static Resources R = GWT.create(Resources.class);
+    private static RoutePointAccessor pointsAccessor = GWT.create(RoutePointAccessor.class);
     
     @UiField
     Window window;
@@ -84,7 +87,7 @@ public class RouteDialog implements MapPointSelectionDialog.PointSelectedListene
     @UiField
     TextField name;
     @UiField(provided = true)
-    Grid<TrackPoint> grid;
+    Grid<RoutePointWrapper> grid;
     @UiField
     FieldLabel selectDeviceLabel;
     @UiField(provided = true)
@@ -95,21 +98,14 @@ public class RouteDialog implements MapPointSelectionDialog.PointSelectedListene
     Messages i18n = GWT.create(Messages.class);
     
     private final Route route;
-    ListStore<TrackPoint> store;
+    ListStore<RoutePointWrapper> store;
     final RouteHandler routeHandler;
-    
-    RegExp latLonPatt = RegExp.compile("(\\d+(\\.\\d+)?)([NS])\\s*(\\d+(\\.\\d+)?)([WE])"); 
     
     public RouteDialog(Route route, final RouteHandler routeHandler, 
             ListStore<Device> devs, ListStore<GeoFence> gfs) {
         this.route = route;
         this.routeHandler = routeHandler;
-        store = new ListStore<>(new ModelKeyProvider<TrackPoint>() {
-            @Override
-            public String getKey(TrackPoint item) {
-                return Integer.toString(item.id);
-            }
-        });
+        store = new ListStore<>(pointsAccessor.id());
         
         prepareGrid(gfs);
         selectDevice = new ComboBox(devs, new LabelProvider<Device>() {
@@ -136,78 +132,32 @@ public class RouteDialog implements MapPointSelectionDialog.PointSelectedListene
         });
         
         name.setValue(route.getName());
-        
-        for(RoutePoint p : route.getRoutePoints()) {
-            TrackPoint tp = new TrackPoint();
-            tp.gf = p.getGeofence();
-            store.add(tp);
-        }
+        for(RoutePoint rp : route.getRoutePoints())
+            store.add(new RoutePointWrapper(rp));
         if(route.getDevice() != null)
             selectDevice.setValue(route.getDevice());
     }
 
     private void prepareGrid(ListStore<GeoFence> gfs) {
-        List<ColumnConfig<TrackPoint, ?>> ccList = new ArrayList<>();
-        ColumnConfig<TrackPoint, String> cName = new ColumnConfig<>(
-                new ValueProvider<TrackPoint, String>() {
-                    @Override
-                    public String getValue(TrackPoint object) {
-                        return object.gf.getName();
-                    }
-                    
-                    @Override
-                    public void setValue(TrackPoint object, String value) {
-                        object.gf.setName(value);
-                    }
-                    
-                    @Override
-                    public String getPath() {
-                        return "name";
-                    }}, 140, i18n.name());
+        List<ColumnConfig<RoutePointWrapper, ?>> ccList = new ArrayList<>();
+        ColumnConfig<RoutePointWrapper, String> cName = new ColumnConfig<>(
+                pointsAccessor.name(), 140, i18n.name());
         ccList.add(cName);
-        ColumnConfig<TrackPoint, String> cAddress = new ColumnConfig<>(
-                new ValueProvider<TrackPoint, String>() {
-                    @Override
-                    public String getValue(TrackPoint object) {
-                        return object.gf.getAddress() == null ? "" : object.gf.getAddress();
-                    }
-                    
-                    @Override
-                    public void setValue(TrackPoint object, String value) {
-                        object.gf.setAddress(value);
-                    }
-                    
-                    @Override
-                    public String getPath() {
-                        return "address";
-                    }}, 330, i18n.address());
+        ColumnConfig<RoutePointWrapper, String> cAddress = new ColumnConfig<>(
+                pointsAccessor.address(), 330, i18n.address());
         ccList.add(cAddress);
-        ColumnConfig<TrackPoint, Integer> cRadius = new ColumnConfig<>(
-                new ValueProvider<TrackPoint, Integer>() {
-                    @Override
-                    public Integer getValue(TrackPoint object) {
-                        return (int)object.gf.getRadius();
-                    }
-                    
-                    @Override
-                    public void setValue(TrackPoint object, Integer value) {
-                        object.gf.setRadius(value);
-                    }
-                    
-                    @Override
-                    public String getPath() {
-                        return "radius";
-                    }}, 50, i18n.radius());
+        ColumnConfig<RoutePointWrapper, Integer> cRadius = new ColumnConfig<>(
+                pointsAccessor.radius(), 50, i18n.radius());
         ccList.add(cRadius);
-        ColumnConfig<TrackPoint, ImageResource> cDelete = new ColumnConfig<>(
-                new ValueProvider<TrackPoint, ImageResource>() {
+        ColumnConfig<RoutePointWrapper, ImageResource> cDelete = new ColumnConfig<>(
+                new ValueProvider<RoutePointWrapper, ImageResource>() {
                     @Override
-                    public ImageResource getValue(TrackPoint object) {
+                    public ImageResource getValue(RoutePointWrapper object) {
                         return R.remove();
                     }
 
                     @Override
-                    public void setValue(TrackPoint object, ImageResource value) {
+                    public void setValue(RoutePointWrapper object, ImageResource value) {
                     }
 
                     @Override
@@ -231,7 +181,7 @@ public class RouteDialog implements MapPointSelectionDialog.PointSelectedListene
             
         });
         ccList.add(cDelete);
-        ColumnModel<TrackPoint> cm = new ColumnModel<>(ccList);
+        ColumnModel<RoutePointWrapper> cm = new ColumnModel<>(ccList);
         grid = new Grid<>(store, cm);
         
         final TextField addr = new TextField();
@@ -239,7 +189,7 @@ public class RouteDialog implements MapPointSelectionDialog.PointSelectedListene
         rad.addValidator(new MaxNumberValidator<>(1500));
         rad.addValidator(new MinNumberValidator<>(300));
         
-        final GridEditing<TrackPoint> edit = new GridInlineEditing<>(grid);
+        final GridEditing<RoutePointWrapper> edit = new GridInlineEditing<>(grid);
         
         List<String> gfNames = new ArrayList<>();
         final Map<String, GeoFence> gfMap= new HashMap<>();
@@ -253,45 +203,22 @@ public class RouteDialog implements MapPointSelectionDialog.PointSelectedListene
         cbName.addValueChangeHandler(new ValueChangeHandler<String>(){
             @Override
             public void onValueChange(ValueChangeEvent<String> event) {
-                if(!gfMap.containsKey(event.getValue()))
-                    return;
-                edit.completeEditing();
-                TrackPoint p = grid.getSelectionModel().getSelectedItem();
-                p.gf = gfMap.get(event.getValue());
+                RoutePointWrapper p = grid.getSelectionModel().getSelectedItem();
+                p.setGeofence(gfMap.get(event.getValue()));
                 store.update(p);
             }
         });
         cbName.setForceSelection(false);
-        
-        addr.addValueChangeHandler(new ValueChangeHandler<String>() {
-            @Override
-            public void onValueChange(ValueChangeEvent<String> e) {
-                MatchResult m = latLonPatt.exec(e.getValue());
-                if(m == null)
-                    return;
-                double lat = Double.parseDouble(m.getGroup(1));
-                if(m.getGroup(3).equals("S"))
-                    lat *= -1;
-                double lon = Double.parseDouble(m.getGroup(4));
-                if(m.getGroup(6).equals("W"))
-                    lon *= -1;
-                store.commitChanges();
-                TrackPoint p = grid.getSelectionModel().getSelectedItem();
-                p.setLonLat(lon, lat);
-                store.update(p);
-            }
-        });
 
         edit.addEditor(cName, cbName);
         edit.addEditor(cAddress, addr);
         edit.addEditor(cRadius, rad);
         
-        edit.addBeforeStartEditHandler(new BeforeStartEditHandler<TrackPoint>() {
+        edit.addBeforeStartEditHandler(new BeforeStartEditHandler<RoutePointWrapper>() {
             @Override
-            public void onBeforeStartEdit(BeforeStartEditEvent<TrackPoint> event) {
-                TrackPoint pt = store.get(event.getEditCell().getRow());
-                if(event.getEditCell().getCol() != 0
-                        && pt != null && pt.gf != null && pt.gf.getId() != 0)
+            public void onBeforeStartEdit(BeforeStartEditEvent<RoutePointWrapper> event) {
+                RoutePointWrapper pt = store.get(event.getEditCell().getRow());
+                if(event.getEditCell().getCol() != 0 && !pt.isEditable())
                     event.setCancelled(true);
             }
         });
@@ -303,7 +230,7 @@ public class RouteDialog implements MapPointSelectionDialog.PointSelectedListene
     
     @UiHandler("addButton")
     public void add(SelectEvent selectEvent) {
-        store.add(new TrackPoint());
+        store.add(new RoutePointWrapper());
     }
     
     @UiHandler("addFromMap")
@@ -313,26 +240,39 @@ public class RouteDialog implements MapPointSelectionDialog.PointSelectedListene
     
     @Override
     public void onPointSelected(LonLat lonLat) {
-        TrackPoint tp = new TrackPoint();
-        tp.setLonLat(lonLat.lon(), lonLat.lat());
-        store.add(tp);
+        RoutePointWrapper pt = new RoutePointWrapper();
+        pt.setLonLat(lonLat.lon(), lonLat.lat());
+        store.add(pt);
     }
     
     @UiHandler("saveButton")
     public void save(SelectEvent selectEvent) {
         store.commitChanges();
-        for(TrackPoint tp : store.getAll()) {
-            RoutePoint rp = new RoutePoint();
-            rp.setGeofence(tp.gf);
-            route.getRoutePoints().add(rp);
-        }
+        route.getRoutePoints().clear();
+        for(RoutePointWrapper rp : store.getAll())
+            route.getRoutePoints().add(rp.getRoutePoint());
         if(connect.getValue()) {
             route.setName(name.getValue());
             route.setDevice(selectDevice.getCurrentValue());
         }
         
-        RouteGenerator g = new RouteGenerator(route, routeHandler, connect.getValue());
-        g.start();
+        if(validate(route, connect.getValue())) {
+            RouteGenerator g = new RouteGenerator(route, routeHandler, connect.getValue());
+            g.start();
+            window.hide();
+        }
+    }
+    
+    private boolean validate(Route r, boolean connect) {
+        if(connect && r.getRoutePoints().size() < 2) {
+            new AlertMessageBox(i18n.error(), "You have to include at least 2 points").show();
+            return false;
+        }
+        if(r.getRoutePoints().size() < 1) {
+            new AlertMessageBox(i18n.error(), "You have to include at least 1 point").show();
+            return false;
+        }
+        return true;
     }
     
     @UiHandler("cancelButton")
@@ -343,6 +283,109 @@ public class RouteDialog implements MapPointSelectionDialog.PointSelectedListene
     
     public static interface RouteHandler {
         void onSave(Route route, boolean connect);
+    }
+    
+    static class RoutePointWrapper {
+        private static final RegExp latLonPatt = RegExp.compile(
+                "(\\d+(\\.\\d+)?)([NS])\\s*(\\d+(\\.\\d+)?)([WE])");
+        
+        private int id;
+        private RoutePoint pt;
+        private static int ID_GEN = 0;
+        
+        public RoutePointWrapper() {
+            id = ID_GEN++;
+            pt = new RoutePoint();
+            pt.setGeofence(createGF("", 300));
+        }
+        
+        public RoutePointWrapper(RoutePoint pt) {
+            id = ID_GEN++;
+            this.pt = pt;
+        }
+        
+        
+        public int getId() {
+            return id;
+        }
+        
+        public String getName() {
+            return pt.getGeofence().getName();
+        }
+        
+        public void setName(String name) {
+            pt.getGeofence().setName(name);
+        }
+        
+        public String getAddress() {
+            return pt.getGeofence().getAddress();
+        }
+        
+        public void setAddress(String address) {
+            pt.getGeofence().setAddress(address);
+            MatchResult m = latLonPatt.exec(address);
+            if(m == null) {
+                return;
+            }
+            double lat = Double.parseDouble(m.getGroup(1)) * 
+                    (m.getGroup(3).equals("S") ? -1 : 1);
+            double lon = Double.parseDouble(m.getGroup(4)) *
+                    (m.getGroup(6).equals("W") ? -1 : 1);
+            pt.setLonLat(lon, lat);
+        }
+        
+        public int getRadius() {
+            return (int)pt.getGeofence().getRadius();
+        }
+        
+        public void setRadius(int radius) {
+            pt.getGeofence().setRadius(radius);
+        }
+        
+        public RoutePoint getRoutePoint() {
+            return pt;
+        }
+        
+        public void setGeofence(GeoFence gf) {
+            if(gf == null)
+                pt.setGeofence(createGF(pt.getGeofence().getName(),
+                        pt.getGeofence().getRadius()));
+            else
+                pt.setGeofence(gf);
+        }
+        
+        public void setLonLat(double lon, double lat) {
+            GeoFence geofence = pt.getGeofence();
+            geofence.setPoints(lon+" "+lat);
+            if(geofence.getAddress() == null || geofence.getAddress().isEmpty()) {
+                String latDir = lat < 0 ? "S" : "N";
+                String lonDir = lon < 0 ? "W" : "E";
+                geofence.setAddress(Math.abs(lat)+latDir+" "+Math.abs(lon)+lonDir);
+            }
+            if(geofence.getName() == null || geofence.getName().isEmpty())
+                geofence.setName("pkt_"+(int)(lon*10)+"_"+(int)(lat*10));
+        }
+        
+        public boolean isEditable() {
+            return pt.getGeofence().getId() == 0;
+        }
+        
+        
+        private GeoFence createGF(String name, float radius) {
+            GeoFence gf = new GeoFence();
+            gf.setName(name);
+            gf.setTransferDevices(Collections.EMPTY_SET);
+            gf.setType(GeoFenceType.CIRCLE);
+            gf.setRadius(radius);
+            return gf;
+        }
+    }
+    
+    interface RoutePointAccessor extends PropertyAccess<RoutePointWrapper> {
+        ModelKeyProvider<RoutePointWrapper> id();
+        ValueProvider<RoutePointWrapper, String> name();
+        ValueProvider<RoutePointWrapper, String> address();
+        ValueProvider<RoutePointWrapper, Integer> radius();
     }
     
     static class RouteGenerator {
@@ -394,37 +437,6 @@ public class RouteDialog implements MapPointSelectionDialog.PointSelectedListene
         
         public void save() {
             routeHandler.onSave(route, connect);
-        }
-    }
-    
-    static class TrackPoint {
-        private static int ID_GEN = 1;
-        
-        int id;
-        GeoFence gf;
-        
-        public TrackPoint() {
-            this(ID_GEN++, "", "", 300);
-        }
-        
-        public TrackPoint(int id, String name, String address, int radius) {
-            this.id = id;
-            this.gf = new GeoFence(0, name);
-            gf.setTransferDevices(Collections.EMPTY_SET);
-            gf.setType(GeoFenceType.CIRCLE);
-            gf.setRadius(300);
-            gf.setAddress("");
-        }
-        
-        public void setLonLat(double lon, double lat) {
-            gf.setPoints(lon+" "+lat);
-            if(gf.getAddress() == null || gf.getAddress().isEmpty()) {
-                String latDir = lat < 0 ? "S" : "N";
-                String lonDir = lon < 0 ? "W" : "E";
-                gf.setAddress(Math.abs(lat)+latDir+" "+Math.abs(lon)+lonDir);
-            }
-            if(gf.getName() == null || gf.getName().isEmpty())
-                gf.setName("pkt_"+(int)(lon*10)+"_"+(int)(lat*10));
         }
     }
     
