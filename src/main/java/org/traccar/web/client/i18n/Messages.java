8950bf3fc030a8ea8d4327cc8476ba5b56ad0fba
DESKTOP-QGMMK31\janusarek <jan.usarek@gmail.com>
2017-12-19 16:14:21 +0100

    Show messageBox after user addition.

--- src/main/java/org/traccar/web/client/i18n/Messages.java
+++ src/main/java/org/traccar/web/client/i18n/Messages.java
@@ -400,6 +400,10 @@ public interface Messages extends com.google.gwt.i18n.client.Messages {
     String errUserSessionExpired();
 
     String errUserDisconnected();
+    
+    String userAddedTitle();
+    
+    String userAddedMessage();
 
     String firstName();
 
