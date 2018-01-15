6ef8ceaf1efdcf4c33328b8cbd0c5e54ca871415
Łukasz Ławniczak <lukasz.lawniczak@datamatica.pl>
2018-01-15 10:42:31 +0100

    RulesDialog UI update

--- src/main/java/org/traccar/web/client/view/RulesDialog.java
+++ src/main/java/org/traccar/web/client/view/RulesDialog.java
@@ -16,6 +16,8 @@
 package org.traccar.web.client.view;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.json.client.JSONValue;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
@@ -25,6 +27,7 @@ import com.google.gwt.user.client.ui.InlineHTML;
 import com.google.gwt.user.client.ui.Widget;
 import com.sencha.gxt.core.client.util.Margins;
 import com.sencha.gxt.widget.core.client.Window;
+import com.sencha.gxt.widget.core.client.button.TextButton;
 import com.sencha.gxt.widget.core.client.container.HorizontalLayoutContainer;
 import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer;
 import com.sencha.gxt.widget.core.client.event.SelectEvent;
@@ -47,7 +50,11 @@ public class RulesDialog {
     Window window;
     @UiField
     VerticalLayoutContainer container;
+    @UiField
+    TextButton acceptButton;
+    
     List<CheckBox> rulesCheckboxes;
+    List<CheckBox> required;
     
     RulesHandler handler;
     boolean force;
@@ -61,19 +68,33 @@ public class RulesDialog {
         this.handler = handler;
         this.force = force;
         this.rulesCheckboxes = new ArrayList<>();
+        this.required = new ArrayList<>();
+        final ValueChangeHandler acceptActivator = new ValueChangeHandler<Boolean>() {
+            @Override
+            public void onValueChange(ValueChangeEvent<Boolean> event) {
+                acceptButton.setEnabled(true);
+                for(CheckBox cb : required)
+                    if(!cb.getValue())
+                        acceptButton.setEnabled(false);
+            }
+        };
         
         uiBinder.createAndBindUi(this);
         for(ApiRulesVersion rv : rvs) {
             HorizontalLayoutContainer hlc = new HorizontalLayoutContainer();            
             CheckBox cb = new CheckBox();
-            cb.setBoxLabel(rv.description + (rv.isObligatory ? " * " : ""));
             cb.setData("id", rv.id);
+            cb.addValueChangeHandler(acceptActivator);
             rulesCheckboxes.add(cb);
-            container.add(cb);
-            InlineHTML p = new InlineHTML("<a href=\""+rv.url+"\">"+"treść tutaj"+"</a>");
-            container.add(p);
-            container.add(hlc, VerticalLayoutContainer.VerticalLayoutData(1, -1, new Margins(0, 8, 0, 8)));
+            if(rv.isObligatory)
+                required.add(cb);
+            hlc.add(cb);
+            InlineHTML p = new InlineHTML("<a href=\""+rv.url+"\">"+rv.description+"</a>" +
+                    (rv.isObligatory ? " *" : ""));
+            hlc.add(p);
+            container.add(hlc, new VerticalLayoutContainer.VerticalLayoutData(1, 20));
         }
+        window.setPixelSize(400, 110+20*rvs.size());
     }
     
     public void show() {
